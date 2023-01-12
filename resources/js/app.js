00509a849007d2ede4226a929d3bf9b25137c4bb
fathalfath30 <fathalfath30@gmail.com>
2023-01-12 22:58:09 +0700

    feat: adding new reactjs scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,40 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
 require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// require('./components/Example');
+class App extends React.Component
+{
+  render()
+  {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card">
+              <div className="card-header">It works!</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const el = document.getElementById("fathalfath30-apps");
+if(el) {
+  ReactDOM.render(<App />, el)
+}
