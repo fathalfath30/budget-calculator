bce2eb50201c04f71250d4f2faaac8d63527cf6f
fathalfath30 <fathalfath30@gmail.com>
2023-05-23 22:32:32 +0700

    feat: create new migration for new four table based on design

---
+++ database/migrations/2023_05_23_133339_create_accounts_table.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\Models\Account;
+use App\Models\Group;
+use App\Models\User;
+use Database\Helper\F30_Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Accounts
+ *
+ * This migration will create table to keep users accounts (not for login) but
+ * to keep basic information such as bank name, card name to identify
+ * transaction history.
+ *
+ * @author Fathalfath30
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * @see \Database\Helper\F30_Migration
+ */
+return new class extends F30_Migration {
+
+  /**
+   * constructor
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function __construct() {
+    $this->table = (new Account())->getTable();
+  }
+
+  /**
+   * Run the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function up() : void {
+    Schema::create($this->getTable(), function(Blueprint $table) {
+      $table->uuid('id');
+      $table->string('name');
+      $table->text('description')
+        ->nullable();
+      $table->uuid('parent_id')
+        ->nullable();
+      $table->uuid('group_id');
+      $table->uuid('user_id')
+        ->nullable();
+
+      // add timestamp
+      $this->addTimestamp($table);
+
+      // set primary key
+      $table->primary('id');
+
+      // adding index
+      $this->addIndex($table, ['name']);
+      $this->addIndex($table, ['parent_id', 'group_id', 'user_id']);
+
+      // adding foreign key
+      $this->addForeign($table, 'parent_id', $this->getTable());
+      $this->addForeign($table, 'group_id', (new Group())->getTable());
+      $this->addForeign($table, 'user_id', (new User())->getTable());
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function down() : void {
+    Schema::dropIfExists($this->getTable());
+  }
+};
---
+++ database/migrations/2023_05_23_141634_create_budget_table.php
@@ -0,0 +1,77 @@
+<?php
+
+use App\Models\Budget;
+use App\Models\User;
+use Database\Helper\F30_Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Budget
+ *
+ * This migration will create "budget" table to keep users current and feature budget
+ * information
+ *
+ * @author Fathalfath30
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * @see \Database\Helper\F30_Migration
+ */
+return new class extends F30_Migration {
+  /**
+   * constructor
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function __construct() {
+    $this->table = (new Budget())->getTable();
+  }
+
+  /**
+   * Run the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function up() : void {
+    Schema::create($this->getTable(), function(Blueprint $table) {
+      $table->char('id', 36);
+      $table->string('name');
+      $table->decimal('amount', 15, 2)->default(0);
+      $table->date('target')
+        ->nullable();
+      $table->uuid('user_id');
+      $table->tinyInteger('budget_type', false, true)
+        ->comment('0: once; 1: daily; 2: weekly; 3: monthly; 4: yearly')
+        ->default(0);
+
+      // add timestamp
+      $this->addTimestamp($table, true);
+
+      // set primary key
+      $table->primary(['id']);
+
+      // add index
+      $this->addIndex($table, ['name', 'budget_type']);
+      $this->addIndex($table, ['user_id']);
+
+      // add foreign key
+      $this->addForeign($table, 'user_id', (new User())->getTable());
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function down() : void {
+    Schema::dropIfExists($this->getTable());
+  }
+};
