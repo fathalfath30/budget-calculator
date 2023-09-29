e75213fb157d24e804273203c49b7fe4440e2b9b
fathalfath30 <fathalfath30@gmail.com>
2023-09-29 21:27:38 +0700

    feat: adding new model and table for user_login_history

---
+++ database/migrations/2023_09_29_142212_create_new_user_login_history_table.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Repository\Models\Budget;
+use App\Repository\Models\User;
+use App\Repository\Models\UserLoginHistory;
+use Database\Helper\F30_Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Budget
+ *
+ * This migration will create "user_login_history" table to store all login history
+ *
+ * @author Fathalfath30
+ * @version 1.0.0
+ * @since 1.0.0
+ *
+ * @see \Database\Helper\F30_Migration
+ */
+return new class extends F30_Migration {
+  /**
+   * constructor
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function __construct() {
+    $this->table = (new UserLoginHistory())->getTable();
+  }
+
+  /**
+   * Run the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function up() : void {
+    Schema::create($this->getTable(), function(Blueprint $table) {
+      $table->uuid('id');
+      $table->uuid('user_id');
+      $table->string('ip_address', 50);
+      $table->text('user_agent')
+        ->nullable();
+      $table->tinyInteger('status', false, true)
+        ->default(0)
+        ->comment('0: failed; 1: success');
+
+      // add timestamp
+      $this->addTimestamp($table);
+
+      // set primary key
+      $table->primary(['id']);
+
+      // add index
+      $this->addIndex($table, ['user_id', 'status']);
+
+      // add foreign key
+      $this->addForeign($table, 'user_id', (new User())->getTable());
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @author Fathalfath30
+   * @version 1.0.0
+   * @since 1.0.0
+   */
+  public function down() : void {
+    Schema::dropIfExists($this->getTable());
+  }
+};
