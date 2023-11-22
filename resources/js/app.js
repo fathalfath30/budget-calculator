e5948bbfcce37e8780925324dac0db6170a198ef
fathalfath30 <fathalfath30@gmail.com>
2023-11-23 01:29:02 +0700

    refactor: adding laravel ui react scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
  */
+
 import './bootstrap';
 
 /**
@@ -10,4 +11,5 @@ import './bootstrap';
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-import './components/Example';
+
+import './components/MainApp.jsx';
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,7 @@ import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
@@ -17,8 +18,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 // import Echo from 'laravel-echo';
+
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
+
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
