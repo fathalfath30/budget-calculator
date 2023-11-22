8c5c2d101e781bcbf52ab32ccb064e4ef466b73a
fathalfath30 <fathalfath30@gmail.com>
2023-11-22 19:31:49 +0700

    refactor: updating mock files

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,6 @@ import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
@@ -18,10 +17,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 // import Echo from 'laravel-echo';
-
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
-
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
